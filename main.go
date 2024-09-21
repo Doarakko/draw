e3c27673a80f431e5a69bb117f42fda766cfe62d
Doarakko <2wua4nlyi@gmail.com>
2024-09-21 13:25:32 +0900

    use http to request yugioh api

--- main.go
+++ main.go
@@ -2,35 +2,77 @@ package main
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
 	"image"
 	_ "image/gif"
 	_ "image/jpeg"
 	_ "image/png"
-	"io/ioutil"
+	"io"
 	"log"
 	"net/http"
 	"os"
 
-	"github.com/Doarakko/go-yugioh/yugioh"
 	"github.com/mattn/go-sixel"
 )
 
-func main() {
-	client := yugioh.NewClient()
+type CardImage struct {
+	ID         int    `json:"id"`
+	URL        string `json:"image_url"`
+	URLSmall   string `json:"image_url_small"`
+	URLCropped string `json:"image_url_cropped"`
+}
 
-	card, _, err := client.RandomCards.One()
+type Card struct {
+	ID     int         `json:"id"`
+	Name   string      `json:"name"`
+	Images []CardImage `json:"card_images"`
+}
+
+type CardResponse struct {
+	Data []Card `json:"data"`
+}
+
+func getYuGiOhCardByRandom() (Card, error) {
+	resp, err := http.Get("https://db.ygoprodeck.com/api/v7/cardinfo.php?num=1&offset=0&sort=random&cachebust")
+	if err != nil {
+		return Card{}, err
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return Card{}, err
+	}
+
+	var cardResponse CardResponse
+	if err := json.Unmarshal(body, &cardResponse); err != nil {
+		return Card{}, err
+	}
+
+	if len(cardResponse.Data) == 0 {
+		return Card{}, fmt.Errorf("card not found")
+	}
+
+	return cardResponse.Data[0], nil
+}
+
+func main() {
+	card, err := getYuGiOhCardByRandom()
 	if err != nil {
 		log.Fatal(err)
+	}
+
+	if len(card.Images) == 0 {
+		log.Fatal("card has no images")
 	}
 
 	resp, err := http.Get(card.Images[0].URL)
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer resp.Body.Close()
 
-	b, err := ioutil.ReadAll(resp.Body)
+	b, err := io.ReadAll(resp.Body)
 	if err != nil {
 		log.Fatal(err)
 	}
